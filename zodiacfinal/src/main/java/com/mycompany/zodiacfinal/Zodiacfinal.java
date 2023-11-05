4c0da3e7c7c80a15ea7eaf4d0f08a045422d9e31
Hanz Uriel A. de la Cruz <144861507+HanzDLC@users.noreply.github.com>
2023-11-06 00:37:19 +0800

    update 8

---
+++ zodiacfinal/src/main/java/com/mycompany/zodiacfinal/Zodiacfinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zodiacfinal;
+
+/**
+ *
+ * @author Records Section
+ */
+public class Zodiacfinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
